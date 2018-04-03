1ff332fcf6c2abdc71cee408640d167128705819
ecodevsolution <ecodevsolution@gmail.com>
2018-04-03 23:44:35 +0700

    update slider dan css home

--- src/components/assets/slider.js
+++ src/components/assets/slider.js
@@ -5,13 +5,49 @@ class Slider extends Component {
     render() {
         return (                        
 
-            <section className="home">                        
-                <h1><b>AP Advocates</b><br/>
-                    A proud Indonesian Intellectual Property Firm<br/>
-                    that is committed to deliver world-class<br/>
-                    Intellectual Property service.
-                </h1>
+            // <section className="home">                        
+            //     <h1><b>AP Advocates</b><br/>
+            //         A proud Indonesian Intellectual Property Firm<br/>
+            //         that is committed to deliver world-class<br/>
+            //         Intellectual Property service.
+            //     </h1>
+            // </section>
+            <section className="home">              
+            <div className="container-fluid no-padding">
+                
+                <div id="myCarousel" className="carousel slide" data-ride="carousel">                
+                    <ol className="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>                          
+                    
+                    <div className="carousel-inner">
+                        <div className="item active">
+                            <img src="img/slider1.jpg" alt="Los Angeles" style={{width:'100%'}} />
+                        </div>            
+
+                        <div className="item">
+                            <img src="img/slider2.jpg" alt="Chicago" style={{width:'100%'}} />
+                        </div>
+
+                        <div className="item">
+                            <img src="img/slider3.jpg" alt="New york" style={{width:'100%'}} />
+                        </div>
+                    </div>          
+                
+                    {/* <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span className="glyphicon glyphicon-chevron-left"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span className="glyphicon glyphicon-chevron-right"></span>
+                        <span className="sr-only">Next</span>
+                    </a> */}
+                </div>
+            </div>
             </section>
+
                 
         );
     }
